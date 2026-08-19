4c5d6819749828c30a7005a9d6683eb84d509145
mauryarajan-782 <mauryarajan782@gmail.com>
2026-08-19 12:23:04 +0530

    Update Employee.java

--- src/main/java/com/hbn/lerning/entity/Employee.java
+++ src/main/java/com/hbn/lerning/entity/Employee.java
@@ -1,78 +1,67 @@
-package com.hbn.lerning.entity;
+package com.hbn.learning.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
-@Entity(name = "emppp")
+
 public class Employee {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String name,gender;
-    private int salary;
-
-    public Employee() {
-
-    }
-
-    public Employee(String name, String gender, int salary) {
-     
-        this.name = name;
-        this.gender = gender;
-        this.salary = salary;
-    }
+	
+	private int id;
+	private String name,gender;
+	private int salary;
+	
+	
+	public Employee() {
+	}
+	
+	public Employee(String name, String gender, int salary) {
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+	}
 
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 
-    public String getGender() {
-        return gender;
-    }
+	public String getGender() {
+		return gender;
+	}
 
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
 
-    public int getSalary() {
-        return salary;
-    }
+	public int getSalary() {
+		return salary;
+	}
 
 
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 
 
-    @Override
-    public String toString() {
-        return "employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
-    }
-
-
-
-
-
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
+	}
+	
 }
