3fc55ad19dd93322760d425bb3271e4726219227
mauryarajan782-design <mauryarajan782@gmail.com>
2026-08-20 00:21:24 +0530

    update

--- src/main/java/com/hbn/learning/Main.java
+++ src/main/java/com/hbn/learning/Main.java
@@ -1,9 +1,7 @@
 package com.hbn.learning;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 import com.hbn.learning.entity.Employee;
 
@@ -12,23 +10,22 @@ public class Main {
 	public static void main(String[] args) {
 
 
-		Employee emp = new Employee("Avinash Jain", "male", 99000,"HCL");
-
-//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-//		SessionFactory sessionFactory = cfg.buildSessionFactory();
-
-//		SessionFactory sessionFactory = HibernateConfig.getSessionFactory();
-
+		Employee emp1 = new Employee("Ashish", "male", 99000,"HCL");
+		
 
 		Session session = HinbernateConfig.getSessionFactory().openSession();
 		Transaction transaction = session.beginTransaction();
 
-		session.persist(emp);
+//		session.persist(emp1);
+//		transaction.commit();
 
-		transaction.commit();
-
-
-
+		
+		Employee employee = session.find(Employee.class, 35);
+		System.out.print(employee);
+		
+		
+		session.load(emp1, 35);
+		System.out.println(emp1);
 	}
 
 }
